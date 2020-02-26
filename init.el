adc381f4c94ccbd24406f9e275684794a48c68ed
yadex <wxn834577101@gmail.com>
2020-02-26 10:38:38 +0800

    upload

--- init.el
+++ init.el
@@ -140,6 +140,7 @@
 (require 'my-ivy)
 (require 'my-org)
 (require 'my-company)
+(require 'org-preview-html)
 
 
 
@@ -168,4 +169,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-table ((t (:family "Iosevka Nerd Font")))))
+ '(ivy-posframe ((t (:background "#161719"))))
+ '(org-table ((t (:family "Iosevka")))))
